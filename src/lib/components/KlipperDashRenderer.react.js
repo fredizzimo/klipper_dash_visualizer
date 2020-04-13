b3f49327eed054b010fb75df1f11492884bed694
Fred Sundvik <fsundvik@gmail.com>
2020-04-13 14:05:15 +0300

    Handle input foucs and resizing

--- src/lib/components/KlipperDashRenderer.react.js
+++ src/lib/components/KlipperDashRenderer.react.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 /**
  * ExampleComponent is an example component.
@@ -10,11 +11,21 @@ import * as THREE from "three";
  * which is editable by the user.
  */
 export default class KlipperDashRenderer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            controls: null,
+            camera: null,
+            renderer: null
+        }
+    }
+
     componentDidMount() {
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        var camera = new THREE.PerspectiveCamera( 75, this.mount.clientWidth/this.mount.clientHeight, 0.1, 1000 );
         var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setSize( this.mount.clientWidth, this.mount.clientHeight );
         // document.body.appendChild( renderer.domElement );
         // use ref as a mount point of the Three.js scene instead of the document.body
         this.mount.appendChild( renderer.domElement );
@@ -29,16 +40,48 @@ export default class KlipperDashRenderer extends Component {
         camera.lookAt(0, 0, 0);
         camera.up.set(0, 0, 1);
 
-        var animate = function () {
-            requestAnimationFrame( animate );
-            renderer.render( scene, camera );
+        var controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableZoom = false;
+        controls.update();
+        this.setState({
+            controls,
+            camera,
+            renderer
+        });
+        window.addEventListener("resize", this.resize);
+
+        var animate = function() {
+            requestAnimationFrame(animate);
+            controls.update()
+            renderer.render(scene, camera);
         };
         animate();
     } 
 
+    enableControls=()=> {
+        this.state.controls.enableZoom = true;
+    }
+    disableControls=()=> {
+        this.state.controls.enableZoom = false;
+    }
+
+    resize=()=> {
+        var camera = this.state.camera;
+        var renderer = this.state.renderer;
+        var mount = this.mount;
+        camera.aspect = mount.clientWidth / mount.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(mount.clientWidth, mount.clientHeight);
+    }
+
     render() {
         return (
-            <div ref={ref => (this.mount = ref)} />
+            <div
+                id={this.props.id}
+                onFocus={this.enableControls}
+                onBlur={this.disableControls}
+                ref={ref => (this.mount = ref)}
+            />
         )
     }
 }
