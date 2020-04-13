54b1d1f97f5991aa823424d1c5c29009d900bb3c
Fred Sundvik <fsundvik@gmail.com>
2020-04-13 18:34:36 +0300

    Draw a buildplate

--- src/lib/components/KlipperDashRenderer.react.js
+++ src/lib/components/KlipperDashRenderer.react.js
@@ -33,12 +33,9 @@ export default class KlipperDashRenderer extends Component {
         var background_color = tinycolor(window.getComputedStyle(domNode).getPropertyValue("background-color"));
         renderer.setClearColor(background_color.toRgb(), background_color.getAlpha());
         this.mount.appendChild( renderer.domElement );
-        var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-        var vertices = new Float32Array(this.props.vertices);
-        var geometry = new THREE.BufferGeometry()
-        geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-        var line = new THREE.Line(geometry, material);
-        scene.add(line);
+
+        this.add_lines(scene);
+        this.add_build_plate(scene);
 
         camera.position.set(0, 0, 300);
         camera.lookAt(0, 0, 0);
@@ -61,6 +58,32 @@ export default class KlipperDashRenderer extends Component {
         };
         animate();
     } 
+
+    add_lines=(scene)=> {
+        var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+        var vertices = new Float32Array(this.props.vertices);
+        var geometry = new THREE.BufferGeometry()
+        geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+        var line = new THREE.Line(geometry, material);
+        scene.add(line);
+    }
+
+    add_build_plate=(scene)=> {
+        var x_dim = this.props.printer_dimensions[0];
+        var y_dim = this.props.printer_dimensions[1];
+
+        var x_size = x_dim[1] - x_dim[0];
+        var y_size = y_dim[1] - y_dim[0];
+        var x_pos = y_dim[0];
+        var y_pos = y_dim[0];
+
+        const thickness = 2
+        var geometry = new THREE.BoxGeometry(x_size, y_size, thickness);
+        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        var plate = new THREE.Mesh(geometry, material);
+        plate.position.set(x_pos + x_size / 2, y_pos + y_size / 2, -thickness/2);
+        scene.add(plate);
+    }
 
     enableControls=()=> {
         this.state.controls.enableZoom = true;
@@ -99,4 +122,5 @@ KlipperDashRenderer.propTypes = {
     id: PropTypes.string,
 
     vertices: PropTypes.arrayOf(PropTypes.number).isRequired,
+    printer_dimensions: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired
 };
