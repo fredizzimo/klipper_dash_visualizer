34d8c5d0167d4a761724ff233a1d3e3de924246f
Fred Sundvik <fsundvik@gmail.com>
2020-04-18 00:08:52 +0300

    Convert helpers to typescript

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,4 +1,4 @@
-function range_start(array, value)
+function range_start(array: ArrayLike<number>, value: number)
 {
     var count = array.length;
     var first = 0;
@@ -22,7 +22,7 @@ function range_start(array, value)
     return first < array.length ? first : array.length-1;
 }
 
-function range_end(array, value)
+function range_end(array: ArrayLike<number>, value: number)
 {
     var count = array.length
     var first = 0;
@@ -42,7 +42,22 @@ function range_end(array, value)
     return first < array.length ? first : array.length-1;
 }
 
-function zoom_trace_y(xvals, yvals, start, end)
+function get_min_max(array: ArrayLike<number>, start: number, end: number) {
+    var min = array[0];
+    var max = array[0] 
+    for (let i=start;i<end;i++) {
+        let v = array[i]
+        if (v < min) {
+            min = v;
+        }
+        else if(v > max) {
+            max = v;
+        }
+    }
+    return [min, max]
+}
+
+function zoom_trace_y(xvals: ArrayLike<number>, yvals: ArrayLike<number>, start: number, end: number)
 {
     if (xvals.length == 0) {
         return [-100, 100];
@@ -51,15 +66,15 @@ function zoom_trace_y(xvals, yvals, start, end)
     var i_high = range_end(xvals, end);
     var num_steps = xvals.length;
     if (i_low >= num_steps) {
-        var range_low = yvals[array.length - 1];
+        var range_low = yvals[yvals.length - 1];
         var range_high = range_low;
     } else if (i_low == i_high) {
         var range_low = yvals[i_low];
         var range_high = range_low;
     } else {
-        var array_range = yvals.slice(i_low, i_high+1);
-        var range_low = Math.min(...array_range);
-        var range_high = Math.max(...array_range);
+        let min_max = get_min_max(yvals, i_low, i_high+1);
+        var range_low = min_max[0];
+        var range_high = min_max[1]
     }
     var diff = range_high - range_low;
     var margin = diff * 0.1;
@@ -68,18 +83,19 @@ function zoom_trace_y(xvals, yvals, start, end)
     return [range_low, range_high];
 }
 
-export function zoom_figure_y(fig)
+export function zoom_figure_y(fig: any)
 {
     var fig = {...fig}
     var x_range = fig.layout.xaxis.range
-    fig.data.forEach((element, i) => {
-       var y_range = zoom_trace_y(element.x, element.y, x_range[0], x_range[1]);
-       var axis_name = "yaxis"
-       if (i > 0) {
-           axis_name = axis_name + (i+1);
-       }
-       fig.layout[axis_name].range = y_range
-       fig.layout[axis_name].autorange = false;
-    });
+    for (let i=0;i<fig.data.length;i++) {
+        let element = fig.data[i];
+        let y_range = zoom_trace_y(element.x, element.y, x_range[0], x_range[1]);
+        let axis_name = "yaxis"
+        if (i > 0) {
+            axis_name = axis_name + (i+1);
+        }
+        fig.layout[axis_name].range = y_range
+        fig.layout[axis_name].autorange = false;
+    }
     return fig
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import KlipperDashRenderer from './components/KlipperDashRenderer.react';
-import {zoom_figure_y} from "./helpers.js"
+import {zoom_figure_y} from "./helpers"
 
 export {
     KlipperDashRenderer,
