9ef8e885083a48fa42294b3d9fe480b9f8440f3c
Fred Sundvik <fsundvik@gmail.com>
2020-07-16 17:12:58 +0300

    Multipler slider resolutions for range_select

--- src/lib/components/range_select.tsx
+++ src/lib/components/range_select.tsx
@@ -6,34 +6,19 @@ import { TimeSlider } from "./time_slider";
 const margin = 2
 const spacing = 2
 
-interface FullSliderProps {
-    min: number
-    max: number
-    value: number
-    onChange: (time: number) => void
-}
-
-const FullSlider: FunctionComponent<FullSliderProps> = (props) => {
-    const full_range_steps = 200
-    return (
-        <TimeSlider
-            min={props.min}
-            max={props.max}
-            value={props.value}
-            onChange={props.onChange}
-            step={(props.max-props.min) / full_range_steps}
-            max_steps={1000}
-        />
-    )
-}
-
 interface Props {
     selected_time: Array<number>;
     min_max_time: Array<number>;
     onTimeSelected : (time: Array<number>) => void;
 }
 
-export const RangeSelect: FunctionComponent<Props> = (props) => {
+interface SliderRowProps extends Props{
+    step?: number
+    num_steps?: number
+    max_steps: number
+}
+
+const SliderRow: FunctionComponent<SliderRowProps> = (props) => {
 
     const onMinTimeChanged=(time: number)=> {
         props.onTimeSelected([time, props.selected_time[1]])
@@ -56,37 +41,87 @@ export const RangeSelect: FunctionComponent<Props> = (props) => {
     const current_time = 0.5 * (start + end)
     const range = end - start
 
+
+    const min=[min_time, min_time + range*0.5, start]
+    const max=[end, max_time - range*0.5, max_time]
+    let step: number[]
+
+    if (props.step == undefined) {
+        const get_num_steps = (min: number, max: number) => {
+            return (max - min) / props.num_steps
+        }
+        step = [
+            get_num_steps(min[0], max[0]),
+            get_num_steps(min[1], max[1]),
+            get_num_steps(min[2], max[2])
+        ]
+    }
+    else {
+        step=[props.step, props.step, props.step]
+    }
+
+    return (
+        <Grid
+            container
+            spacing={spacing}
+        >
+            <Grid item xs={4}>
+                <TimeSlider
+                    min={min[0]}
+                    max={max[0]}
+                    value={start}
+                    onChange={onMinTimeChanged}
+                    step={step[0]}
+                    max_steps={props.max_steps}
+                />
+            </Grid>
+            <Grid item xs={4}>
+                <TimeSlider
+                    min={min[1]}
+                    max={max[1]}
+                    value={current_time}
+                    onChange={onCurrentTimeChanged}
+                    step={step[1]}
+                    max_steps={props.max_steps}
+                />
+            </Grid>
+            <Grid item xs={4}>
+                <TimeSlider
+                    min={min[2]}
+                    max={max[2]}
+                    value={end}
+                    onChange={onMaxTimeChanged}
+                    step={step[2]}
+                    max_steps={props.max_steps}
+                />
+            </Grid>
+        </Grid>
+    )
+}
+
+export const RangeSelect: FunctionComponent<Props> = (props) => {
     return (
         <Box m={margin}>
-            <Grid
-                container
-                spacing={spacing}
-            >
-                <Grid item xs={4}>
-                    <FullSlider
-                        min={min_time}
-                        max={end}
-                        value={start}
-                        onChange={onMinTimeChanged}
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <FullSlider
-                        min={min_time + range*0.5}
-                        max={max_time - range*0.5}
-                        value={current_time}
-                        onChange={onCurrentTimeChanged}
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <FullSlider
-                        min={start}
-                        max={max_time}
-                        value={end}
-                        onChange={onMaxTimeChanged}
-                    />
-                </Grid>
-            </Grid>
+            <SliderRow
+                {...props}
+                num_steps={200}
+                max_steps={1000}
+            /> 
+            <SliderRow
+                {...props}
+                step={30}
+                max_steps={120}
+            /> 
+            <SliderRow
+                {...props}
+                step={0.5}
+                max_steps={120}
+            /> 
+            <SliderRow
+                {...props}
+                step={0.01}
+                max_steps={100}
+            /> 
         </Box>
     )
 }
