56356d3678fa8763fea7158ffcdb4be83d3b4d74
Fred Sundvik <fsundvik@gmail.com>
2020-07-22 17:42:44 +0300

    Completed implementation and tests for linear fixed ticks scale

--- src/lib/linear_fixed_ticks_scale.test.ts
+++ src/lib/linear_fixed_ticks_scale.test.ts
@@ -1,5 +1,10 @@
 import { scaleLinearFixedTicks } from "./linear_fixed_ticks_scale"
-import { assertThat, equalTo } from "hamjest"
+import { assertThat, equalTo, closeTo, contains, not, lessThanOrEqualTo, greaterThanOrEqualTo } from "hamjest"
+import * as ld from "lodash"
+
+const almostEqTo = (value: number[]) => { 
+    return ld.map(value, (value: number) => {return closeTo(value, 1e-12)})
+}
 
 describe("linearFixedTicksScale", () => {
     it("initializes the default nice values", () => {
@@ -15,16 +20,25 @@ describe("linearFixedTicksScale", () => {
         assertThat(ticks.length, equalTo(10))
         assertThat(ticks, equalTo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     })
+    test("copy works correctly", () => {
+        const scale1 = scaleLinearFixedTicks().domain([9, 17]).niceValues([0.1, 0.5])
+        const scale2 = scale1.copy()
+        assertThat(scale1, not(equalTo(scale2)))
+        assertThat(scale2.domain(), equalTo(scale1.domain()))
+        assertThat(scale2.niceValues(), equalTo(scale2.niceValues()))
+        scale1.niceValues([0.1, 0.4])
+        assertThat(scale2.niceValues(), equalTo([0.1, 0.5]))
+    })
     it("returns the desired number of ticks", () => {
         const ticks = scaleLinearFixedTicks().domain([1, 3]).ticks(3)
         assertThat(ticks.length, equalTo(3))
         assertThat(ticks, equalTo([1, 2, 3]))
     })
-    test("a 10 step range with integer steps", () => {
+    test("a 10 tick range with integer steps", () => {
         const ret = scaleLinearFixedTicks().domain([1, 10]).ticks(10)
         assertThat(ret, equalTo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     })
-    test("a 3 step range with integer steps", () => {
+    test("a 3 tick range with integer steps", () => {
         const ret = scaleLinearFixedTicks().domain([1, 3]).ticks(3)
         assertThat(ret, equalTo([1, 2, 3]))
     })
@@ -32,8 +46,103 @@ describe("linearFixedTicksScale", () => {
         const ret = scaleLinearFixedTicks().domain([2, 5]).ticks(4)
         assertThat(ret, equalTo([2, 3, 4, 5]))
     })
-    it("divide 0 to 1 into five steps should match exactly", () => {
+    test("an integer range starting from negative going to positive", () => {
+        const ret = scaleLinearFixedTicks().domain([-2, 1]).ticks(4)
+        assertThat(ret, equalTo([-2, -1, 0, 1]))
+    })
+    test("a negative integer range", () => {
+        const ret = scaleLinearFixedTicks().domain([-6, -2]).ticks(5)
+        assertThat(ret, equalTo([-6, -5, -4, -3, -2]))
+    })
+    test("divide 0 to 1 into five ticks should match exactly", () => {
         const ret = scaleLinearFixedTicks().domain([0, 1]).ticks(5)
-        assertThat(ret, equalTo([0, 0.25, 0.5, 0.75, 1.0]))
+        const expected = almostEqTo([0, 0.25, 0.5, 0.75, 1.0])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 0 to 0.5 into 7 ticks should extend the range", () => {
+        const ret = scaleLinearFixedTicks().domain([0, 0.5]).ticks(7)
+        const expected = almostEqTo([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+        assertThat(ret, contains(...expected))
+    })
+    test("extended range also changes the domain", () => {
+        const scale = scaleLinearFixedTicks()
+        scale.domain([0, 0.5]).ticks(7)
+        const expected = almostEqTo([0, 0.6])
+        assertThat(scale.domain(), contains(...expected))
+    })
+    test("divide -0.5 to 0 into 7 ticks should extend the range", () => {
+        const ret = scaleLinearFixedTicks().domain([-0.5, 0.0]).ticks(7)
+        const expected = almostEqTo([-0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 0 to 100 into five ticks should match exactly", () => {
+        const ret = scaleLinearFixedTicks().domain([0, 100]).ticks(5)
+        const expected = almostEqTo([0, 25, 50, 75, 100])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 20 to 110 into ten ticks should match exactly", () => {
+        const ret = scaleLinearFixedTicks().domain([20, 110]).ticks(10)
+        const expected = almostEqTo([20, 30, 40, 50, 60, 70, 80, 90, 100, 110])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 30 to 110 into ten ticks should extend the upper range", () => {
+        const ret = scaleLinearFixedTicks().domain([30, 110]).ticks(10)
+        const expected = almostEqTo([30, 40, 50, 60, 70, 80, 90, 100, 110, 120])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 0.0 to 0.1 into 6 ticks should match exactly", () => {
+        const ret = scaleLinearFixedTicks().domain([0, 0.1]).ticks(6)
+        const expected = almostEqTo([0.0, 0.02, 0.04, 0.06, 0.08, 0.1])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 10 to 100 into five ticks should shift the lower end", () => {
+        const ret = scaleLinearFixedTicks().domain([10, 100]).ticks(5)
+        const expected = almostEqTo([0, 25, 50, 75, 100])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide -90 to 0 into five ticks should shift the lower end", () => {
+        const ret = scaleLinearFixedTicks().domain([-90, 0]).ticks(5)
+        const expected = almostEqTo([-100, -75, -50, -25, 0])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide -40 to 50 into five ticks should shift the lower end", () => {
+        const ret = scaleLinearFixedTicks().domain([-40, 50]).ticks(5)
+        const expected = almostEqTo([-50, -25, 0, 25, 50])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 10 to 110 into five ticks should shift the lower and increase the step", () => {
+        const ret = scaleLinearFixedTicks().domain([10, 110]).ticks(5)
+        const expected = almostEqTo([0, 30, 60, 90, 120])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide -110 to -10 into five ticks should shift the lower and increase the step", () => {
+        const ret = scaleLinearFixedTicks().domain([-110, -10]).ticks(5)
+        const expected = almostEqTo([-120, -90, -60, -30, 0])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 1 to 3 into two ticks should shift and increase the step by two", () => {
+        const ret = scaleLinearFixedTicks().domain([1, 3]).ticks(2)
+        const expected = almostEqTo([0, 3])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 1 to 9 into two ticks should shift and increase the step", () => {
+        const ret = scaleLinearFixedTicks().domain([1, 9]).ticks(2)
+        const expected = almostEqTo([0, 10])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 1 to 11 into two ticks should shift and increase the step", () => {
+        const ret = scaleLinearFixedTicks().domain([1, 11]).ticks(2)
+        const expected = almostEqTo([0, 12])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide 1 to 11 into two ticks with nice values not starting from 0.1", () => {
+        const ret = scaleLinearFixedTicks().domain([1, 11]).niceValues([0.2, 0.7, 0.9]).ticks(2)
+        const expected = almostEqTo([0, 20])
+        assertThat(ret, contains(...expected))
+    })
+    test("divide -10 to 116 into ten ticks should extend both ends", () => {
+        const ret = scaleLinearFixedTicks().domain([-10, 116]).ticks(10)
+        const expected = almostEqTo([-15, 0, 15, 30, 45, 60, 75, 90, 105, 120])
+        assertThat(ret, contains(...expected))
     })
 })
--- src/lib/linear_fixed_ticks_scale.ts
+++ src/lib/linear_fixed_ticks_scale.ts
@@ -1,21 +1,66 @@
-import { scaleLinear, ScaleLinear, range as d3_range } from "d3";
+import { scaleLinear, ScaleLinear } from "d3";
 
 interface ScaleLinearFixedTicks extends ScaleLinear<number, number> {
     niceValues(values: number[]) : this
     niceValues() : number[]
 }
-export function scaleLinearFixedTicks() : ScaleLinearFixedTicks {
-    let linear_scale = scaleLinear() as ScaleLinearFixedTicks
-    let nice_values: number[] = [0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8]
-    const original_tick_function = linear_scale.ticks
+function scaleLinearFixedTicksImplementation(linear_scale_in: ScaleLinear<number, number>, nice_values: number[]) : ScaleLinearFixedTicks {
+    let linear_scale = linear_scale_in as ScaleLinearFixedTicks 
+    const original_copy = linear_scale.copy
+
+    linear_scale.copy = () => {
+        let ret = original_copy()
+        let niceValues = [...linear_scale.niceValues()]
+        return scaleLinearFixedTicksImplementation(ret, niceValues)
+    } 
+
     linear_scale.ticks = (count?: number): number[] => {
         if (count == undefined) {
             count = 10
         }
         const domain = linear_scale.domain()
         const range = domain[1] - domain[0]
-        const step = range / (count - 1)
-        return d3_range(domain[0], domain[1] + step/2, step)
+        const preliminary_step = range / (count -1 )
+        let step_scale = 10**Math.ceil(Math.log10(preliminary_step))
+
+        let step = (preliminary_step) / step_scale
+        let i=0
+        for (;i<nice_values.length;++i) {
+            if (step <= nice_values[i]) {
+                step = nice_values[i]
+                break
+            }
+        }
+        if (i==nice_values.length) {
+            step = 1.0;
+        }
+        step*= step_scale
+        let start = Math.floor(domain[0] / step)*step
+        let end = start + step*(count-1)
+        while (end < domain[1]) {
+            i++
+            if (i==nice_values.length) {
+                step = step_scale
+            }
+            else if(i>nice_values.length) {
+                step_scale*=10
+                if (nice_values[0]==0.1) {
+                    i=1
+                }
+                else {
+                    i=0
+                }
+                step = nice_values[i] * step_scale
+            }
+            else {
+                step = nice_values[i] * step_scale
+            }
+            start = Math.floor(domain[0] / step)*step
+            end = start + step*(count-1)
+        }
+        linear_scale.domain([start, end])
+
+        return Array.from({length: count}, (_, i: number) => {return start + i*step})
     }
     linear_scale.niceValues = (values?: number[]): any => {
         if (values == undefined) {
@@ -26,5 +71,11 @@ export function scaleLinearFixedTicks() : ScaleLinearFixedTicks {
             return  linear_scale
         }
     }
-    return linear_scale as ScaleLinearFixedTicks
+    return linear_scale
+}
+
+export function scaleLinearFixedTicks() : ScaleLinearFixedTicks {
+    let linear_scale = scaleLinear()
+    let nice_values: number[] = [0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8]
+    return scaleLinearFixedTicksImplementation(linear_scale, nice_values)
 }
