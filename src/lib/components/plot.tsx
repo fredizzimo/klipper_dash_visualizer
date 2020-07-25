190fc107a81a7b92ccf0053977002007e80693d1
Fred Sundvik <fsundvik@gmail.com>
2020-07-25 19:10:33 +0300

    Convert plot to a class based component
    Juding from a lot of reading, the react hooks just have so many problems
    and is a poor desing IMO.

--- src/lib/components/plot.tsx
+++ src/lib/components/plot.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent, useEffect, useRef, useState, useCallback } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState, useCallback, Component, createRef } from "react";
 import * as fc from "d3fc"
 import * as d3 from "d3" 
 import { Theme, makeStyles } from "@material-ui/core";
-import { WithStyles, withStyles, useTheme } from "@material-ui/styles";
+import { WithStyles, withStyles, useTheme, createStyles } from "@material-ui/styles";
 import * as ld from "lodash"
 import {range_start, range_end, get_min_max} from "../helpers"
 import {scaleLinearFixedTicks, ScaleLinearFixedTicks} from "../linear_fixed_ticks_scale"
@@ -31,7 +31,7 @@ const trace_colors = [
     d3.color("rgb(23, 190, 207)"),
 ]
 
-const useStyles = makeStyles((theme: Theme) => ({
+const styles = (theme: Theme) => createStyles({
     container: {
         display: "grid",
         width: "100%",
@@ -61,7 +61,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     yaxis_line: {
         stroke: "black",
     }
-}));
+});
 
 type Trace = {
     name: string,
@@ -74,12 +74,16 @@ export type PlotDef = {
     times: Array<number>;
 }
 
-interface Props {
+interface Props extends WithStyles<typeof styles>{
     plot: PlotDef
     selected_time: Array<number>
 }
 
-class PlotImpl {
+type State = {
+
+}
+
+class PlotImpl extends Component<Props, State> {
     extent = fc.extentLinear();
 
     xScale = d3.scaleLinear()
@@ -89,126 +93,33 @@ class PlotImpl {
         .tickSize(axis_tick_size)
         .tickPadding(axis_tick_padding)
 
+    plot: PlotDef
+    selected_time: number[]
+
     series: any[]
     y_scales: ScaleLinearFixedTicks[]
     y_axis: any
-    container: any = null;
+    main_canvas_ref = createRef<any>()
+    x_axis_ref = createRef<any>()
 
-    render = (props: Props) => {
-        const x_axis = useRef<any>()
+    componentDidMount() {
+        this.renderCanvas()
+        this.main_canvas_ref.current.requestRedraw()
+    }
 
-        const main_canvas = useCallback((container: any) => {
-            this.container = container
-            d3.select(container)
-                .on('measure', () => {
-                    container.querySelector("canvas")
-                    const { width, height, pixelRatio} = d3.event.detail;
-                    this.xScale.range([0, width / pixelRatio ]);
-                    for (let i=0;i<this.y_scales.length;i++) {
-                        this.y_scales[i].range([height / pixelRatio, 0]);
-                    }
+    componentDidUpdate() {
+        this.main_canvas_ref.current.requestRedraw()
+    }
 
-                    const gl = container.querySelector('canvas').getContext('webgl');
-                    for (let i=0;i<this.series.length;i++) {
-                        this.series[i].context(gl);
-                    }
-                })
-                .on('draw', () => {
-                    for (let i=0;i<this.series.length;i++) {
-                        this.series[i](props.plot.traces[i].data);
-                    }
-                    d3.select(x_axis.current)
-                        .select("svg")
-                        .call(this.xAxis)
-                });
-        }, [])
-
-        const initialize_plot = () => {
-            props.plot = props.plot
-            const traces = props.plot.traces
-            this.series = new Array(traces.length)
-            this.y_scales = new Array<ScaleLinearFixedTicks>(traces.length)
-            for(let i=0;i<traces.length;i++) {
-                const y_scale = scaleLinearFixedTicks().niceValues(
-                    [0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 0.9]
-                )
-
-                const series = fc
-                    .seriesWebglLine()
-                    .xScale(this.xScale)
-                    .yScale(y_scale)
-                    .crossValue((_: any, i: number) => props.plot.times[i])
-                    .mainValue((d: number) => d)
-                    .defined(() => true)
-                    .equals((previousData: Array<number>) => previousData.length > 0)
-                    .decorate((program: any) => {
-                        const {r, g, b, opacity} = trace_colors[i].rgb()
-                        fc.webglStrokeColor([r / 255, g / 255, b / 255, opacity])(program)
-                    })
-
-                this.y_scales[i] = y_scale
-                this.series[i] = series
-            }
-            this.y_axis = fc.axisLeft(this.y_scales[0])
-                .ticks(y_axis_ticks)
-                .tickSize(axis_tick_size)
-                .tickPadding(axis_tick_padding)
+    render() {
+        if (this.plot != this.props.plot) {
+            this.initialize_plot()
+        }
+        if (this.selected_time != this.props.selected_time) {
+            this.updateSelectedTime()
         }
 
-        const updateSelectedTime = () => {
-            this.xScale.domain(props.selected_time)
-            zoomTraces()
-            this.container.requestRedraw()
-        }
-
-        const zoomTraces = () => {
-            const time = this.xScale.domain()
-            const times = props.plot.times
-            for (let trace=0;trace<props.plot.traces.length;trace++) {
-                const data = props.plot.traces[trace].data
-                const range = getTraceYRange(times, data, time[0], time[1])
-                // Also call ticks here, so that the actual used domain is set
-                this.y_scales[trace].domain(range).ticks(y_axis_ticks)
-            }
-        }
-
-        const getTraceYRange = (xvals: ArrayLike<number>, yvals: ArrayLike<number>, start: number, end: number) => {
-            const min_diff = 1e-12
-
-            if (xvals.length == 0) {
-                return [-min_diff, min_diff];
-            }
-            var i_low = range_start(xvals, start);
-            var i_high = range_end(xvals, end);
-            var num_steps = xvals.length;
-            if (i_low >= num_steps) {
-                var range_low = yvals[yvals.length - 1];
-                var range_high = range_low;
-            } else if (i_low == i_high) {
-                var range_low = yvals[i_low];
-                var range_high = range_low;
-            } else {
-                let min_max = get_min_max(yvals, i_low, i_high+1);
-                var range_low = min_max[0];
-                var range_high = min_max[1]
-            }
-            var diff = range_high - range_low;
-            if (diff < min_diff) {
-                range_high += min_diff
-            }
-            return [range_low, range_high];
-        }
-
-        useEffect(() => {
-            initialize_plot()
-        }, [props.plot])
-
-
-        useEffect(() => {
-            updateSelectedTime()
-        }, [props.selected_time, props.plot])
-
-        const styles = useStyles()
+        const styles = this.props.classes
 
         return (
             <div>
@@ -217,7 +128,7 @@ class PlotImpl {
                         use-device-pixel-ratio
                         set-webgl-viewport
                         class={styles.graph}
-                        ref={main_canvas}
+                        ref={this.main_canvas_ref}
                     />
                     <svg
                         className={styles.yaxis}
@@ -231,18 +142,117 @@ class PlotImpl {
                     </svg>
                     <d3fc-svg
                         class={styles.xaxis}
-                        ref={x_axis}
+                        ref={this.x_axis_ref}
                     />
                 </div>
             </div>
         ) 
     }
+
+    renderCanvas() {
+        const canvas = this.main_canvas_ref.current
+        d3.select(canvas)
+            .on('measure', () => {
+                canvas.querySelector("canvas")
+                const { width, height, pixelRatio} = d3.event.detail;
+                this.xScale.range([0, width / pixelRatio ]);
+                for (let i=0;i<this.y_scales.length;i++) {
+                    this.y_scales[i].range([height / pixelRatio, 0]);
+                }
+
+                const gl = canvas.querySelector('canvas').getContext('webgl');
+                for (let i=0;i<this.series.length;i++) {
+                    this.series[i].context(gl);
+                }
+            })
+            .on('draw', () => {
+                for (let i=0;i<this.series.length;i++) {
+                    this.series[i](this.props.plot.traces[i].data);
+                }
+                d3.select(this.x_axis_ref.current)
+                    .select("svg")
+                    .call(this.xAxis)
+            });
+    }
+
+    initialize_plot() {
+        const props = this.props
+        const traces = props.plot.traces
+        this.plot = props.plot
+        this.selected_time = null
+        this.series = new Array(traces.length)
+        this.y_scales = new Array<ScaleLinearFixedTicks>(traces.length)
+        for(let i=0;i<traces.length;i++) {
+            const y_scale = scaleLinearFixedTicks().niceValues(
+                [0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 0.9]
+            )
+
+            const series = fc
+                .seriesWebglLine()
+                .xScale(this.xScale)
+                .yScale(y_scale)
+                .crossValue((_: any, i: number) => props.plot.times[i])
+                .mainValue((d: number) => d)
+                .defined(() => true)
+                .equals((previousData: Array<number>) => previousData.length > 0)
+                .decorate((program: any) => {
+                    const {r, g, b, opacity} = trace_colors[i].rgb()
+                    fc.webglStrokeColor([r / 255, g / 255, b / 255, opacity])(program)
+                })
+
+            this.y_scales[i] = y_scale
+            this.series[i] = series
+        }
+        this.y_axis = fc.axisLeft(this.y_scales[0])
+            .ticks(y_axis_ticks)
+            .tickSize(axis_tick_size)
+            .tickPadding(axis_tick_padding)
+    }
+
+    updateSelectedTime() {
+        this.selected_time = this.props.selected_time
+        this.xScale.domain(this.props.selected_time)
+        this.zoomTraces()
+    }
+
+    zoomTraces() {
+        const props = this.props
+        const time = this.xScale.domain()
+        const times = props.plot.times
+        for (let trace=0;trace<props.plot.traces.length;trace++) {
+            const data = props.plot.traces[trace].data
+            const range = this.getTraceYRange(times, data, time[0], time[1])
+            // Also call ticks here, so that the actual used domain is set
+            this.y_scales[trace].domain(range).ticks(y_axis_ticks)
+        }
+    }
+
+    getTraceYRange (xvals: ArrayLike<number>, yvals: ArrayLike<number>, start: number, end: number) {
+        const min_diff = 1e-12
+
+        if (xvals.length == 0) {
+            return [-min_diff, min_diff];
+        }
+        var i_low = range_start(xvals, start);
+        var i_high = range_end(xvals, end);
+        var num_steps = xvals.length;
+        if (i_low >= num_steps) {
+            var range_low = yvals[yvals.length - 1];
+            var range_high = range_low;
+        } else if (i_low == i_high) {
+            var range_low = yvals[i_low];
+            var range_high = range_low;
+        } else {
+            let min_max = get_min_max(yvals, i_low, i_high+1);
+            var range_low = min_max[0];
+            var range_high = min_max[1]
+        }
+        var diff = range_high - range_low;
+        if (diff < min_diff) {
+            range_high += min_diff
+        }
+        return [range_low, range_high];
+    }
 }
 
-export const Plot: FunctionComponent<Props> = (props) => {
-        const impl = useRef<PlotImpl>()
-        if (impl.current == null) {
-            impl.current = new PlotImpl()
-        }
-        return impl.current.render(props)
-}
+export const Plot = withStyles(styles)(PlotImpl)
